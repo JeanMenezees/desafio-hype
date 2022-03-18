b5314637433151fee2e81b65ee4ce103481840d7
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-03-18 03:32:15 -0300

    Removendo consolelog desnecessário

--- src/components/PaginaInicial/Inicio.js
+++ src/components/PaginaInicial/Inicio.js
@@ -22,7 +22,6 @@ function App() {
   useEffect(() => {
     construirLista()
       .then((produtos) => {
-        console.log(produtos);
         setProdutos(produtos);
       })
       .then(() => setCarregando(false));
